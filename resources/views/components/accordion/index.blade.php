1a783f403407dcd96a58cf0fbff55eb4dfe5d7a1
Dennis Okon <deokon@gmail.com>
2026-01-14 17:33:12 -0500

    feat: implement Accordion component

---
+++ resources/views/components/accordion/index.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'alwaysOpen' => false,
+])
+
+<div 
+    x-data="{ 
+        active: null,
+        alwaysOpen: {{ $alwaysOpen ? 'true' : 'false' }},
+        select(id) {
+            if (this.alwaysOpen) return;
+            this.active = (this.active === id) ? null : id;
+        }
+    }" 
+    {{ $attributes->merge(['class' => 'divide-y divide-background-700/40 dark:divide-background-400/20 border-y border-background-700/40 dark:border-background-400/20']) }}
+>
+    {{ $slot }}
+</div>
---
+++ resources/views/components/accordion/item.blade.php
@@ -0,0 +1,50 @@
+@props([
+    'title',
+    'id' => \Illuminate\Support\Str::random(8),
+    'open' => false,
+])
+
+<div 
+    x-data="{ 
+        id: '{{ $id }}',
+        get isOpen() {
+            if (this.alwaysOpen) return this.localOpen;
+            return this.active === this.id;
+        },
+        set isOpen(value) {
+            if (this.alwaysOpen) {
+                this.localOpen = value;
+            } else {
+                this.select(this.id);
+            }
+        },
+        localOpen: {{ $open ? 'true' : 'false' }}
+    }"
+    x-init="if(localOpen && !alwaysOpen) active = id"
+    {{ $attributes->merge(['class' => 'group']) }}
+>
+    <h3>
+        <button
+            type="button"
+            class="flex w-full items-center justify-between py-4 text-left font-medium transition-colors hover:text-primary focus:outline-none"
+            @click="isOpen = !isOpen"
+            :aria-expanded="isOpen"
+        >
+            <span>{{ $title }}</span>
+            <x-plume::icon 
+                i="icon-[fluent--chevron-down-24-regular]" 
+                class="size-4 transition-transform duration-200" 
+                ::class="isOpen ? 'rotate-180' : ''" 
+            />
+        </button>
+    </h3>
+    <div 
+        x-show="isOpen" 
+        x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="opacity-0 -translate-y-1"
+        x-transition:enter-end="opacity-100 translate-y-0"
+        class="pb-4 text-foreground/70 dark:text-background-400"
+    >
+        {{ $slot }}
+    </div>
+</div>
