ee548d4b59db658d1894b441c319ae0cc2ddd0cd
Dennis Okon <deokon@gmail.com>
2026-01-14 15:00:52 -0500

    feat: add logo component and use it as breadcrumb separator

--- resources/views/components/breadcrumb/separator.blade.php
+++ resources/views/components/breadcrumb/separator.blade.php
@@ -1,6 +1,6 @@
 <li role="presentation" aria-hidden="true" {{ $attributes->merge(['class' => '[&>.icon]:size-2.5']) }}>
     {{ $slot ?? '' }}
     @if(!$slot->isNotEmpty())
-        <x-plume::icon i="icon-[fluent--chevron-right-24-regular]" />
+        <x-plume::logo size="size-2.5" class="text-foreground/30" />
     @endif
 </li>
