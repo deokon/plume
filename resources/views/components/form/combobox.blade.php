d1b365c30acf6089c07478f35d6b9d87ec174ddd
Dennis Okon <deokon@gmail.com>
2026-01-14 17:54:54 -0500

    fix: improve Combobox reactivity and option handling

--- resources/views/components/form/combobox.blade.php
+++ resources/views/components/form/combobox.blade.php
@@ -5,35 +5,51 @@
     'options' => [], // Array of ['value' => '...', 'label' => '...']
 ])
 
-<x-plume::form.element :label="$label" :model="$model" {{ $attributes }}>
+<x-plume::form.element :label="$label" :model="$model" {{ $attributes->only('class') }}>
     <div 
         x-data="{ 
             open: false,
             search: '',
-            value: @if($model) $wire.entangle('{{ $model }}') @else '' @endif,
-            options: {{ json_encode($options) }},
+            value: null,
+            localOptions: [],
             get filteredOptions() {
-                if (this.search === '') return this.options;
-                return this.options.filter(opt => 
-                    opt.label.toLowerCase().includes(this.search.toLowerCase())
+                if (this.search === '') return this.localOptions;
+                return this.localOptions.filter(opt => 
+                    String(opt.label).toLowerCase().includes(this.search.toLowerCase())
                 );
             },
             get selectedLabel() {
-                const opt = this.options.find(o => o.value == this.value);
+                const opt = this.localOptions.find(o => o.value == this.value);
                 return opt ? opt.label : '';
             },
             select(option) {
                 this.value = option.value;
+                @if($model) 
+                    if (typeof $data.{{ $model }} !== 'undefined') {
+                        $data.{{ $model }} = option.value;
+                    }
+                @endif
                 this.search = '';
                 this.open = false;
             }
         }"
+        x-init="
+            localOptions = $data.options || {{ json_encode($options) }};
+            @if($model)
+                if (typeof $data.{{ $model }} !== 'undefined') {
+                    $watch('$data.{{ $model }}', val => value = val);
+                    value = $data.{{ $model }};
+                }
+            @endif
+            $watch('open', val => { if(val) $nextTick(() => $refs.searchInput.focus()) })
+        "
         class="relative"
         @click.away="open = false"
     >
         {{-- Input --}}
         <div class="relative">
             <input 
+                x-ref="searchInput"
                 type="text" 
                 class="block w-full pl-3 pr-10 py-2 border border-background-700/40 dark:border-background-400/20 rounded-lg bg-background placeholder:text-foreground/30 focus:outline-none focus:ring-2 focus:ring-primary/50 focus:border-primary transition-all sm:text-sm cursor-default"
                 :placeholder="selectedLabel || '{{ $placeholder }}'"
@@ -53,6 +69,7 @@
             x-transition:enter-start="opacity-0 scale-95"
             x-transition:enter-end="opacity-100 scale-100"
             class="absolute z-50 w-full mt-1 bg-background border border-background-700/40 dark:border-background-400/20 rounded-lg shadow-lg overflow-hidden"
+            style="display: none;"
         >
             <ul class="max-h-60 overflow-y-auto p-1">
                 <template x-for="option in filteredOptions" :key="option.value">
@@ -64,9 +81,9 @@
                         <span x-text="option.label"></span>
                     </li>
                 </template>
-                <li x-show="filteredOptions.length === 0" class="px-3 py-2 text-sm text-foreground/40 text-center">
+                <div x-show="filteredOptions.length === 0" class="px-3 py-2 text-sm text-foreground/40 text-center py-4">
                     No results found.
-                </li>
+                </div>
             </ul>
         </div>
     </div>
