618a6e9522217f48813a6f3c5e2895a65f3dd059
Dennis Okon <deokon@gmail.com>
2026-01-14 17:33:44 -0500

    feat: implement Figure component

---
+++ resources/views/components/figure.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'src',
+    'alt' => '',
+    'caption' => null,
+    'aspect' => null,
+])
+
+<figure {{ $attributes->merge(['class' => 'overflow-hidden rounded-lg bg-background-100 dark:bg-background-800']) }}>
+    <div @class([
+        'relative w-full',
+        match($aspect) {
+            'square' => 'aspect-square',
+            'video' => 'aspect-video',
+            '4/3' => 'aspect-[4/3]',
+            '3/2' => 'aspect-[3/2]',
+            '21/9' => 'aspect-[21/9]',
+            default => '',
+        }
+    ])>
+        <img 
+            src="{{ $src }}" 
+            alt="{{ $alt }}" 
+            class="h-full w-full object-cover"
+            loading="lazy"
+        >
+    </div>
+    @if($caption || $slot->isNotEmpty())
+        <figcaption class="p-3 text-sm text-foreground/60 dark:text-background-400">
+            {{ $caption ?? $slot }}
+        </figcaption>
+    @endif
+</figure>
