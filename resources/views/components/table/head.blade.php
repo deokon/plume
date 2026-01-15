86a5343af6d99184185ca3757e3579d2fa2f69ca
Dennis Okon <deokon@gmail.com>
2026-01-15 10:26:33 -0500

    fix(table): make sorting indicators reactive using AlpineJS

--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -7,18 +7,27 @@
 ])
 
 <th {{ $attributes->merge(['class' => 'h-12 px-4 text-left align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0' . ($sortable ? ' cursor-pointer select-none hover:bg-background-200/50 dark:hover:bg-background-700/50' : '')]) }}>
-    <div class="flex items-center gap-2">
+    <div 
+        class="flex items-center gap-2"
+        @if($sortable)
+            x-data="{ 
+                get dir() { return this.$el.closest('th').getAttribute('direction') } 
+            }"
+        @endif
+    >
         {{ $slot }}
 
         @if($sortable)
-            <div class="flex flex-col text-foreground/30">
+            <div class="flex flex-col text-foreground/20 shrink-0">
                 <x-plume::icon 
                     i="icon-[fluent--chevron-up-24-regular]" 
-                    class="size-3 -mb-1 {{ $direction === 'asc' ? 'text-primary' : '' }}" 
+                    class="size-3 -mb-1"
+                    ::class="dir === 'asc' ? 'text-primary' : ''"
                 />
                 <x-plume::icon 
                     i="icon-[fluent--chevron-down-24-regular]" 
-                    class="size-3 -mt-1 {{ $direction === 'desc' ? 'text-primary' : '' }}" 
+                    class="size-3 -mt-1"
+                    ::class="dir === 'desc' ? 'text-primary' : ''"
                 />
             </div>
         @endif
