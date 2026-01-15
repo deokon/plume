db8d2e12bd6dd0d05d0eab646b1c03b71742264c
Dennis Okon <deokon@gmail.com>
2026-01-15 10:28:16 -0500

    fix(table): ensure sorting icons are visible and reactive

--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -6,28 +6,25 @@
     'direction' => null, // 'asc', 'desc', or null
 ])
 
+@php
+    $directionValue = $attributes->get('::direction') ?? (isset($direction) ? "'$direction'" : 'null');
+@endphp
+
 <th {{ $attributes->merge(['class' => 'h-12 px-4 text-left align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0' . ($sortable ? ' cursor-pointer select-none hover:bg-background-200/50 dark:hover:bg-background-700/50' : '')]) }}>
-    <div 
-        class="flex items-center gap-2"
-        @if($sortable)
-            x-data="{ 
-                get dir() { return this.$el.closest('th').getAttribute('direction') } 
-            }"
-        @endif
-    >
-        {{ $slot }}
+    <div class="flex items-center gap-2">
+        <span>{{ $slot }}</span>
 
         @if($sortable)
-            <div class="flex flex-col text-foreground/20 shrink-0">
+            <div class="flex flex-col text-foreground/40 shrink-0">
                 <x-plume::icon 
                     i="icon-[fluent--chevron-up-24-regular]" 
-                    class="size-3 -mb-1"
-                    ::class="dir === 'asc' ? 'text-primary' : ''"
+                    class="size-3 -mb-1 transition-colors"
+                    ::class="({{ $directionValue }}) === 'asc' ? 'text-primary opacity-100' : ''"
                 />
                 <x-plume::icon 
                     i="icon-[fluent--chevron-down-24-regular]" 
-                    class="size-3 -mt-1"
-                    ::class="dir === 'desc' ? 'text-primary' : ''"
+                    class="size-3 -mt-1 transition-colors"
+                    ::class="({{ $directionValue }}) === 'desc' ? 'text-primary opacity-100' : ''"
                 />
             </div>
         @endif
