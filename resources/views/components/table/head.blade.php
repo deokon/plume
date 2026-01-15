195afc2e8c5768724ea27be00a6c525ce2bbe6cf
Dennis Okon <deokon@gmail.com>
2026-01-15 10:32:33 -0500

    fix(table): ensure icons render in dynamic templates by checking for bound attributes

--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -3,13 +3,19 @@
 --}}
 @props([
     'sortable' => false,
-    'direction' => null, // 'asc', 'desc', or null
+    'direction' => null,
 ])
 
 @php
-    $hasSortAttribute = $attributes->has('sortable') || $attributes->has('::sortable') || $attributes->has('x-bind:sortable');
-    $isSortable = $sortable || $hasSortAttribute;
+    // Check for any sorting-related attributes to ensure server-side rendering of markup
+    $isSortable = $sortable 
+        || $attributes->has('sortable') 
+        || $attributes->has('::sortable') 
+        || $attributes->has('x-bind:sortable')
+        || $attributes->has('::direction');
+
     $directionValue = $attributes->get('::direction') ?? (isset($direction) ? "'$direction'" : 'null');
+    $sortableValue = $attributes->get('::sortable') ?? $attributes->get('x-bind:sortable') ?? ($isSortable ? 'true' : 'false');
 @endphp
 
 <th {{ $attributes->merge(['class' => 'h-12 px-4 text-left align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0' . ($isSortable ? ' cursor-pointer select-none hover:bg-background-200/50 dark:hover:bg-background-700/50' : '')]) }}>
@@ -18,21 +24,17 @@
 
         @if($isSortable)
             <div 
-                class="flex flex-col text-foreground/40 shrink-0"
-                @if($hasSortAttribute)
-                    x-show="{{ $attributes->get('::sortable') ?? $attributes->get('x-bind:sortable') ?? 'true' }}"
-                @endif
+                class="flex flex-col text-foreground/30 shrink-0"
+                x-show="{{ $sortableValue }}"
             >
-                <x-plume::icon 
-                    i="icon-[fluent--chevron-up-24-regular]" 
-                    class="size-3 -mb-1 transition-colors"
-                    ::class="({{ $directionValue }}) === 'asc' ? 'text-primary opacity-100' : ''"
-                />
-                <x-plume::icon 
-                    i="icon-[fluent--chevron-down-24-regular]" 
-                    class="size-3 -mt-1 transition-colors"
-                    ::class="({{ $directionValue }}) === 'desc' ? 'text-primary opacity-100' : ''"
-                />
+                <span 
+                    class="icon icon-[fluent--chevron-up-24-regular] size-3 -mb-1 transition-colors"
+                    :class="({{ $directionValue }}) === 'asc' ? 'text-primary opacity-100' : ''"
+                ></span>
+                <span 
+                    class="icon icon-[fluent--chevron-down-24-regular] size-3 -mt-1 transition-colors"
+                    :class="({{ $directionValue }}) === 'desc' ? 'text-primary opacity-100' : ''"
+                ></span>
             </div>
         @endif
     </div>
