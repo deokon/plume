085c878830e65aeaaa9c57437bfa78ade271bbc7
Dennis Okon <deokon@gmail.com>
2026-01-15 10:42:00 -0500

    table styles

--- resources/views/components/table/header.blade.php
+++ resources/views/components/table/header.blade.php
@@ -1,6 +1,6 @@
 {{--
 @component x-plume::table.header
 --}}
-<thead {{ $attributes->merge(['class' => '[&_tr]:border-b']) }}>
+<thead {{ $attributes->merge(['class' => '[&_tr]:border-b-2']) }}>
     {{ $slot }}
 </thead>
