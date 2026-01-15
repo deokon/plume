a211c4b0b68ff03d0a08ecd313bf2d93370e23f9
Dennis Okon <deokon@gmail.com>
2026-01-15 10:30:52 -0500

    fix(table): ensure sort icons render when using dynamic AlpineJS bindings

--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -7,15 +7,22 @@
 ])
 
 @php
+    $hasSortAttribute = $attributes->has('sortable') || $attributes->has('::sortable') || $attributes->has('x-bind:sortable');
+    $isSortable = $sortable || $hasSortAttribute;
     $directionValue = $attributes->get('::direction') ?? (isset($direction) ? "'$direction'" : 'null');
 @endphp
 
-<th {{ $attributes->merge(['class' => 'h-12 px-4 text-left align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0' . ($sortable ? ' cursor-pointer select-none hover:bg-background-200/50 dark:hover:bg-background-700/50' : '')]) }}>
+<th {{ $attributes->merge(['class' => 'h-12 px-4 text-left align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0' . ($isSortable ? ' cursor-pointer select-none hover:bg-background-200/50 dark:hover:bg-background-700/50' : '')]) }}>
     <div class="flex items-center gap-2">
         <span>{{ $slot }}</span>
 
-        @if($sortable)
-            <div class="flex flex-col text-foreground/40 shrink-0">
+        @if($isSortable)
+            <div 
+                class="flex flex-col text-foreground/40 shrink-0"
+                @if($hasSortAttribute)
+                    x-show="{{ $attributes->get('::sortable') ?? $attributes->get('x-bind:sortable') ?? 'true' }}"
+                @endif
+            >
                 <x-plume::icon 
                     i="icon-[fluent--chevron-up-24-regular]" 
                     class="size-3 -mb-1 transition-colors"
