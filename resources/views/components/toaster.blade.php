cb31ccc3b744b14f8e98addf203c49f9aaa72adf
Dennis Okon <deokon@gmail.com>
2026-01-16 15:16:52 -0500

    Update toaster.blade.php

--- resources/views/components/toaster.blade.php
+++ resources/views/components/toaster.blade.php
@@ -33,7 +33,7 @@
     class="fixed {{ $positionClasses }} z-50 flex flex-col gap-2 p-4 sm:p-6 max-h-screen overflow-hidden pointer-events-none"
     x-data
 >
-    <template x-for="toast in $store.toasts.items.filter(t => (t.position || 'bottom-right') === '{{ $position }}')" :key="toast.id">
+    <template x-for="toast in $store.toasts.items" :key="toast.id">
         <div
             x-transition:enter="transition ease-out duration-300"
             x-transition:enter-start="{{ $enterStart }}"
