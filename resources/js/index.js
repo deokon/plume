49f324aa818e794741673e6a54c67445aa814b4c
Dennis Okon <deokon@gmail.com>
2026-01-16 14:23:34 -0500

    Calendar abstraction

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,5 +4,6 @@ import page from './alpine/page';
 import clipboard from './alpine/clipboard';
 import pagination from './alpine/pagination';
 import dataTable from './alpine/data-table';
+import calendar from './alpine/calendar';
 
-export { modal, toaster, page, clipboard, pagination, dataTable };
+export { modal, toaster, page, clipboard, pagination, dataTable, calendar };
