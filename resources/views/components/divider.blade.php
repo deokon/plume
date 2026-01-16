50f62492240fabbbc8dd94d3a697345a5e556e4e
Dennis Okon <deokon@gmail.com>
2026-01-16 14:23:38 -0500

    Update divider.blade.php

--- resources/views/components/divider.blade.php
+++ resources/views/components/divider.blade.php
@@ -7,12 +7,12 @@
 <div {{ $attributes->merge(['class' => 'relative flex items-center py-5']) }}>
     <div class="flex-grow border-t border-background-700/40 dark:border-background-400/20"></div>
     @if($label || $slot->isNotEmpty())
-        <div class="mx-4 flex items-center gap-2 flex-shrink-0 text-foreground/30 dark:text-background-400/50">
+        <div class="mx-4 flex items-center gap-2 shrink-0 text-foreground/30 dark:text-background-400/50">
             @if($label)
                 <span class="text-xs font-semibold uppercase tracking-widest">{{ $label }}</span>
             @endif
             {{ $slot }}
         </div>
     @endif
-    <div class="flex-grow border-t border-background-700/40 dark:border-background-400/20"></div>
+    <div class="grow border-t border-background-700/40 dark:border-background-400/20"></div>
 </div>
