3456a3ecf10473c278fea1fb7938a2701a0beb07
Dennis Okon <deokon@gmail.com>
2026-01-17 16:22:04 -0500

    refactor: remove constructed text alignment classes in table

--- resources/views/components/table/cell.blade.php
+++ resources/views/components/table/cell.blade.php
@@ -2,7 +2,16 @@
 @component x-plume::table.cell
 --}}
 @props(['align' => 'left'])
+
+@php
+    $alignClass = match ($align) {
+        'center' => 'text-center',
+        'right' => 'text-right',
+        default => 'text-left',
+    };
+@endphp
+
 <td
-    {{ $attributes->merge(['class' => "p-4 align-middle text-$align [&:has([role=checkbox])]:pr-0"]) }}>
+    {{ $attributes->merge(['class' => "p-4 align-middle $alignClass [&:has([role=checkbox])]:pr-0"]) }}>
     {{ $slot }}
 </td>
--- resources/views/components/table/head.blade.php
+++ resources/views/components/table/head.blade.php
@@ -2,7 +2,16 @@
 @component x-plume::table.head
 --}}
 @props(['align' => 'left'])
+
+@php
+    $alignClass = match ($align) {
+        'center' => 'text-center',
+        'right' => 'text-right',
+        default => 'text-left',
+    };
+@endphp
+
 <th
-    {{ $attributes->merge(['class' => "h-12 px-4 text-$align align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0"]) }}>
+    {{ $attributes->merge(['class' => "h-12 px-4 $alignClass align-middle uppercase font-medium [&:has([role=checkbox])]:pr-0"]) }}>
     {{ $slot }}
 </th>
