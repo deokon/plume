f5da963376b3b8a485747d208c6eb9ae97e43454
Dennis Okon <deokon@gmail.com>
2026-01-19 13:46:17 -0500

    refactor(tooltip): standardize themeStyles pattern

--- src/View/Components/Tooltip.php
+++ src/View/Components/Tooltip.php
@@ -15,29 +15,33 @@ class Tooltip extends Component
 
     public function render(): View|Closure|string
     {
+        $styles = $this->themeStyles();
+
         return view('plume::components-class.tooltip', [
-            'positionClasses' => $this->positionClasses(),
-            'arrowClasses' => $this->arrowClasses(),
+            'positionClasses' => $styles['position'],
+            'arrowClasses' => $styles['arrow'],
         ]);
     }
 
-    public function positionClasses(): string
+    protected function themeStyles(): array
     {
-        return match ($this->position) {
+        $positions = [
             'bottom' => 'top-full left-1/2 -translate-x-1/2 mt-2',
             'left' => 'right-full top-1/2 -translate-y-1/2 mr-2',
             'right' => 'left-full top-1/2 -translate-y-1/2 ml-2',
-            default => 'bottom-full left-1/2 -translate-x-1/2 mb-2', // top
-        };
-    }
+            'top' => 'bottom-full left-1/2 -translate-x-1/2 mb-2',
+        ];
 
-    public function arrowClasses(): string
-    {
-        return match ($this->position) {
+        $arrows = [
             'bottom' => 'bottom-full left-1/2 -translate-x-1/2 border-b-background-800 dark:border-b-background-200 border-x-transparent border-t-transparent',
             'left' => 'left-full top-1/2 -translate-y-1/2 border-l-background-800 dark:border-l-background-200 border-y-transparent border-r-transparent',
             'right' => 'right-full top-1/2 -translate-y-1/2 border-r-background-800 dark:border-r-background-200 border-y-transparent border-l-transparent',
-            default => 'top-full left-1/2 -translate-x-1/2 border-t-background-800 dark:border-t-background-200 border-x-transparent border-b-transparent', // top
-        };
+            'top' => 'top-full left-1/2 -translate-x-1/2 border-t-background-800 dark:border-t-background-200 border-x-transparent border-b-transparent',
+        ];
+
+        return [
+            'position' => $positions[$this->position] ?? $positions['top'],
+            'arrow' => $arrows[$this->position] ?? $arrows['top'],
+        ];
     }
 }
