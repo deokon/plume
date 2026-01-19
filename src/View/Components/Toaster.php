fdf6c6dee0a48f3c4fabb62f4dab182096a57f06
Dennis Okon <deokon@gmail.com>
2026-01-19 13:45:00 -0500

    refactor(toaster): standardize themeStyles pattern

--- src/View/Components/Toaster.php
+++ src/View/Components/Toaster.php
@@ -14,40 +14,44 @@ class Toaster extends Component
 
     public function render(): View|Closure|string
     {
+        $styles = $this->themeStyles();
+
         return view('plume::components-class.toaster', [
-            'positionClasses' => $this->positionClasses(),
-            'enterStart' => $this->enterStart(),
+            'positionClasses' => $styles['position'],
+            'enterStart' => $styles['enter_start'],
             'enter' => $this->transitions('overlay-enter'),
             'leave' => $this->transitions('overlay-leave'),
         ]);
     }
 
-    public function positionClasses(): string
+    protected function themeStyles(): array
     {
-        return match ($this->position) {
+        $positions = [
             'top-left' => 'top-0 left-0 items-start',
             'top-center' => 'top-0 left-1/2 -translate-x-1/2 items-center',
             'top-right' => 'top-0 right-0 items-end',
             'bottom-left' => 'bottom-0 left-0 items-start',
             'bottom-center' => 'bottom-0 left-1/2 -translate-x-1/2 items-center',
             'bottom-right' => 'bottom-0 right-0 items-end',
-            default => 'bottom-0 right-0 items-end',
-        };
-    }
+        ];
 
-    public function enterStart(): string
-    {
+        $position = $this->position;
         $enterStart = 'translate-y-2 opacity-0';
-        if (str_contains($this->position, 'center')) {
+
+        if (str_contains($position, 'center')) {
             // Center: keep vertical slide (translate-y-2), no horizontal
-        } elseif (str_contains($this->position, 'right')) {
+        } elseif (str_contains($position, 'right')) {
             // Right: Reset Y, slide from right
             $enterStart .= ' sm:translate-y-0 sm:translate-x-2';
         } else {
             // Left: Reset Y, slide from left
             $enterStart .= ' sm:translate-y-0 sm:-translate-x-2';
         }
-        return $enterStart;
+
+        return [
+            'position' => $positions[$position] ?? $positions['bottom-right'],
+            'enter_start' => $enterStart,
+        ];
     }
 
     protected function transitions(string $type = 'default'): string
