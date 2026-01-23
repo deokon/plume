89e7cb9b69569268eed04044d99811b9501dbe9c
Dennis Okon <deokon@gmail.com>
2026-01-23 11:10:53 -0500

    Update index.blade.php

--- resources/views/components-class/accordion/index.blade.php
+++ resources/views/components-class/accordion/index.blade.php
@@ -10,6 +10,6 @@
 </x-plume::accordion>
 --}}
 <div x-data="accordion({{ $alwaysOpen ? 'true' : 'false' }})"
-    {{ $attributes->merge(['class' => 'divide-y divide-background-700/40 dark:divide-background-400/20 border-y border-background-700/40 dark:border-background-400/20']) }}>
+    {{ $attributes->merge(['class' => 'divide-y divide-background-700/40 dark:divide-background-400/20 border-y border-background-700/40 dark:border-background-400/20 w-full']) }}>
     {{ $slot }}
 </div>
