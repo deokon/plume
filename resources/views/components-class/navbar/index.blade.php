99e5ba30c2b68c6bc8b082458a58de1bf63e8913
Dennis Okon <deokon@gmail.com>
2026-01-23 07:36:14 -0500

    fix(navbar): prevent slot content from squishing by using shrink-0 and spacer

--- resources/views/components-class/navbar/index.blade.php
+++ resources/views/components-class/navbar/index.blade.php
@@ -5,9 +5,9 @@
 <nav x-data="{ mobileOpen: false }"
     {{ $attributes->merge(['class' => 'bg-background border-b border-background-200 dark:border-background-800' . ($sticky ? ' sticky top-0 z-50' : '')]) }}>
     <div class="mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex h-16 justify-between items-center">
+        <div class="flex h-16 justify-between items-center gap-4">
             @if(isset($left) || isset($center) || isset($right))
-                <div class="flex flex-1 items-center justify-start h-full">
+                <div class="flex shrink-0 items-center justify-start h-full">
                     {{ $left ?? '' }}
                 </div>
 
@@ -15,9 +15,11 @@
                     <div class="flex flex-1 items-center justify-center h-full text-center">
                         {{ $center }}
                     </div>
+                @else
+                    <x-plume::spacer />
                 @endif
 
-                <div class="flex flex-1 items-center justify-end h-full">
+                <div class="flex shrink-0 items-center justify-end h-full gap-2">
                     {{ $right ?? '' }}
                     @if(isset($mobile))
                         <x-plume::navbar.mobile-toggle :icon="$mobileIcon" class="ml-2" />
