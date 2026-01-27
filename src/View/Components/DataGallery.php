3bd6aebaedb3d9e8b4c0ee1774c2e26a07ee7d57
Dennis Okon <deokon@gmail.com>
2026-01-27 06:54:17 -0500

    refactor: use literal Tailwind classes for gap in DataGallery

--- src/View/Components/DataGallery.php
+++ src/View/Components/DataGallery.php
@@ -50,6 +50,20 @@ class DataGallery extends Component
             default => 'grid-cols-1 sm:grid-cols-2 lg:grid-cols-3',
         };
 
-        return $gridCols . ' gap-' . $this->gap;
+        $gapClass = match ($this->gap) {
+            0 => 'gap-0',
+            1 => 'gap-1',
+            2 => 'gap-2',
+            3 => 'gap-3',
+            4 => 'gap-4',
+            5 => 'gap-5',
+            6 => 'gap-6',
+            8 => 'gap-8',
+            10 => 'gap-10',
+            12 => 'gap-12',
+            default => 'gap-4',
+        };
+
+        return $gridCols . ' ' . $gapClass;
     }
 }
